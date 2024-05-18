5b2d38d60e28643d4d89a6bb1b722e6a6e9a0fc8
Noma <wiostz98kr@gmail.com>
2024-05-18 15:59:03 +0900

    Fix: Fix YoutubeApiProvider import error

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import SearchHeader from './components/SearchHeader';
-import { YoutubeApiProvider } from './context/YoutubeApiContext';
+import { YoutubeApiProvider } from './context/YoutubeProvider';
 
 const queryClient = new QueryClient();
 
