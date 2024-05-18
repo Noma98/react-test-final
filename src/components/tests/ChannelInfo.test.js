47b76c627ddc72bce3738d5dccd0499017aa0a33
Noma <wiostz98kr@gmail.com>
2024-05-18 10:56:54 +0900

    Test: Add unit tests for ChannelInfo

--- src/components/tests/ChannelInfo.test.js
+++ src/components/tests/ChannelInfo.test.js
@@ -12,13 +12,42 @@ describe('ChannelInfo', () => {
 
   it('renders corretly', async () => {
     fakeYoutube.channelImageUrl.mockImplementation(() => 'url');
+    const { asFragment } = render(
+      withAllContexts(
+        withRouter(
+          <Route path='/' element={<ChannelInfo id='id' name='channel' />} />
+        ),
+        fakeYoutube
+      )
+    );
+    await waitFor(() => screen.getByRole('img'));
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('renders without URL', () => {
+    fakeYoutube.channelImageUrl.mockImplementation(() => {
+      throw new Error('error');
+    });
     render(
       withAllContexts(
         withRouter(
           <Route path='/' element={<ChannelInfo id='id' name='channel' />} />
-        )
+        ),
+        fakeYoutube
       )
     );
-    await waitFor(() => screen.getByText('channel'));
+    expect(screen.queryByRole('img')).toBeNull();
+  });
+  it('renders with URL', async () => {
+    fakeYoutube.channelImageUrl.mockImplementation(() => 'url');
+    render(
+      withAllContexts(
+        withRouter(
+          <Route path='/' element={<ChannelInfo id='id' name='channel' />} />
+        ),
+        fakeYoutube
+      )
+    );
+    await waitFor(() => expect(screen.getByRole('img')).toBeInTheDocument());
   });
 });
